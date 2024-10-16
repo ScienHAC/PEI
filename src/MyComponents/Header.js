2d5b47344d7d19cb826e52793776cbc8f50b33cd
Piyush Sharma <90610178+ScienHAC@users.noreply.github.com>
2024-10-16 20:49:53 +0530

    "make changes to the Header component"

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 export default function Header() {
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        navigate('/login'); // This will navigate to the /about route
+    };
+    const handleSignup = () => {
+        navigate('/signup'); // This will navigate to the /about route
+    };
     return (
         <>
             <div id="topBar">
@@ -9,10 +16,8 @@ export default function Header() {
                     ISSN: 0923--4748 Scopus Indexed and Multidisciplinary Journal
                 </p>
             </div>
-            <nav
-                className="navbar navbar-expand-lg navbar-light mx-auto"
-                style={{ backgroundColor: "#e3f2fd" }}
-            >
+
+            <nav className="navbar navbar-expand-lg navbar-light mx-auto" style={{ backgroundColor: "#e3f2fd" }}>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -24,49 +29,54 @@ export default function Header() {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
+                {/* Buttons that stay outside the collapsible part */}
+                <div className="d-flex ms-auto d-lg-none">
+                    <button className="btn btn-outline-success mx-2" id="Login_btn" onClick={handleLogin}>
+                        Login
+                    </button>
+                    <button className="btn btn-outline-success mx-2" id="Signup_btn" onClick={handleSignup}>
+                        Sign Up
+                    </button>
+                </div>
+
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav me-auto">
                         <li className="nav-item active">
-                            <Link className="nav-link" to="/">
-                                Welcome to PEI<span className="sr-only">(current)</span>
-                            </Link>
+                            <Link className="nav-link" to="/">Welcome to PEI<span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">
-                                About
-                            </Link>
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="google.com">
-                                Abstracting & Indexing
-                            </Link>
+                            <Link className="nav-link" to="google.com">Abstracting & Indexing</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link disabled" to="google.com">
-                                Current Issue
-                            </Link>
+                            <Link className="nav-link disabled" to="google.com">Current Issue</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="google.com">
-                                Archives
-                            </Link>
+                            <Link className="nav-link" to="google.com">Archives</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="google.com">
-                                Editorial Board
-                            </Link>
+                            <Link className="nav-link" to="google.com">Editorial Board</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="google.com">
-                                Contact Us
-                            </Link>
+                            <Link className="nav-link" to="google.com">Contact Us</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/form">
-                                Research Archive
-                            </Link>
+                            <Link className="nav-link" to="/form">Research Archive</Link>
                         </li>
                     </ul>
+
+                    {/* Buttons on the right for larger screens */}
+                    <div className="d-none d-lg-flex">
+                        <button className="btn btn-outline-success mx-2" id="Login_btn" onClick={handleLogin}>
+                            Login
+                        </button>
+                        <button className="btn btn-outline-success mx-2" id="Signup_btn" onClick={handleSignup}>
+                            Sign Up
+                        </button>
+                    </div>
                 </div>
             </nav>
         </>
