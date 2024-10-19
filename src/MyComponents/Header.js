875dfcf2c03807ef3bdc09bb39894ac690e7f65d
Piyush Sharma <90610178+ScienHAC@users.noreply.github.com>
2024-10-19 11:34:06 +0530

    "add dasboard for user only"

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -110,9 +110,13 @@ export default function Header() {
                         <li className="nav-item">
                             <Link className="nav-link" to="/form">Research Archive</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
-                        </li>
+                        {isAuthenticated ? (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                            </li>
+                        ) : (
+                            <></>
+                        )}
                     </ul>
 
                     {/* Buttons on the right for larger screens */}
